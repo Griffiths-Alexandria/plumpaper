f37d308a17c602b40a84f40ba8c49c79b4b3c2bf
plumpaper <plumpaper@192.168.0.11>
2016-02-12 11:38:54 -0800

    about page content waiting on photos

--- about.php
+++ about.php
@@ -25,15 +25,20 @@ and open the template in the editor.
             <div class="upperdivider">
                 <img src="images/home_divider_line.png" alt="divider">
             </div>
-
             
-
-            <div id="roundbuttons">
-                <a href="#"><img src="images/home_circle_1.png" alt="homecirleplanner" id="round1"></a>
-                <a href="#"><img src="images/home_circle_2.png" alt="homecirleaddon" id="round2"></a>                
-                <a href="#"><img src="images/home_circle_3.png" alt="homecirleshop" id="round3"></a>
+            <div class="header">
+                <img src="images/aboutheader.png" alt="divider">
             </div>
-
+            
+            <div id="abouttxt">
+            <p class="font">
+            <strong> Plum Paper </strong> is a stationery company based in San Diego, CA. While starting out making simple note cards and wedding invitations, it wasn’t long before our line of planners and notebooks became our most popular and favorite product. It has been so exciting to see the joy these planners can bring to people. Never did we think that creating these personalized products would receive such a positive response, and we love to hear the continuous exciting feedback from our customers.
+                <br>
+                <br>
+Each of our products are handmade from start to finish right here in our San Diego office. We put our heart and hard work into each one of our products, and are proud to share these items with you. We hope that they not only help you stay organized throughout the year, but also keep you smiling with pretty patterns and colors.
+            </p>
+            </div>
+            
             <div id="footer">
                 <?php include("modules/footer.php"); ?>
             </div>
--- contact.php
+++ contact.php
@@ -26,13 +26,6 @@ and open the template in the editor.
                 <img src="images/home_divider_line.png" alt="divider">
             </div>
 
-            
-
-            <div id="roundbuttons">
-                <a href="#"><img src="images/home_circle_1.png" alt="homecirleplanner" id="round1"></a>
-                <a href="#"><img src="images/home_circle_2.png" alt="homecirleaddon" id="round2"></a>                
-                <a href="#"><img src="images/home_circle_3.png" alt="homecirleshop" id="round3"></a>
-            </div>
 
             <div id="footer">
                 <?php include("modules/footer.php"); ?>
